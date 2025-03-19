123e9cbc7de1b56decf1f5ac27ce7c054c5bc7f1
Nada <nrsamara20@gmail.com>
2025-03-19 04:42:46 +0300

    Add Prettier configuration and formatted code

--- backend/src/utils/dbTest.js
+++ backend/src/utils/dbTest.js
@@ -3,7 +3,9 @@ const db = require('./db');
 
 async function testConnection() {
   try {
-    const [rows] = await db.execute('SELECT " Connection successful" AS status');
+    const [rows] = await db.execute(
+      'SELECT " Connection successful" AS status'
+    );
     console.log(rows[0].status);
   } catch (error) {
     console.error(' Connection failed:', error.message);
